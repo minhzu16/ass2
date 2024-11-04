03f97e0667936bbd27de6683d7d0591a62341722
minhzu16 <minhvu160105@gmail.com>
2024-11-04 10:44:13 +0700

    update main de in trong so

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 import java.util.Arrays;
 import java.util.List;
 
-public class main {
+public class Main {
     public static void main(String[] args) {
         Graph graph = new Graph();
-        // Thêm các cạnh cho các điểm A - F
+
+        // Thêm các cạnh cho các điểm A - Z
         graph.addEdge("A", "B", 5);
         graph.addEdge("A", "C", 10);
         graph.addEdge("B", "C", 2);
@@ -34,29 +35,39 @@ public class main {
         graph.addEdge("X", "Y", 8);
         graph.addEdge("Y", "Z", 3);
 
+        // Tìm đường đi ngắn nhất từ A đến Z
         List<String> shortestPath = graph.findShortestPath("A", "Y");
-        System.out.println("Shortest path from A to Z : " + shortestPath);
+        System.out.println("Shortest path from A to Y: " + shortestPath);
+        System.out.println();
+
         // Kiểm tra trọng số của đường đi
-        int totalWeight = graph.checkSameWeightPath(shortestPath);
+        int totalWeight = graph.getPathWeight(shortestPath);
         System.out.println("Total weight of the shortest path: " + totalWeight);
+        System.out.println();
 
         // Tối ưu đường đi giữa nhiều điểm có thứ tự
         List<String> orderedPoints = Arrays.asList("A", "B", "D", "F");
-        List<String> optimizedOrderedPath = graph.optimizeRouteOrdered(orderedPoints);
-        System.out.println("Optimized ordered path: " + optimizedOrderedPath);
+        Pair<List<String>, Integer> optimizedOrderedResult = graph.optimizeRouteOrdered(orderedPoints);
+        System.out.println("Optimized ordered path: " + optimizedOrderedResult.getKey());
+        System.out.println("Total weight of ordered path: " + optimizedOrderedResult.getValue());
+        System.out.println();
 
         // Tối ưu đường đi giữa nhiều điểm không có thứ tự
         List<String> unorderedPoints = Arrays.asList("A", "B", "C", "D", "E", "F");
-        List<String> optimizedUnorderedPath = graph.optimizeRouteUnordered(unorderedPoints);
-        System.out.println("Optimized unordered path: " + optimizedUnorderedPath);
+        Pair<List<String>, Integer> optimizedUnorderedResult = graph.optimizeRouteUnordered(unorderedPoints);
+        System.out.println("Optimized unordered path: " + optimizedUnorderedResult.getKey());
+        System.out.println("Total weight of unordered path: " + optimizedUnorderedResult.getValue());
+        System.out.println();
 
         // Kiểm tra vòng lặp trong đường đi
         List<String> pathWithCircle = Arrays.asList("A", "B", "C", "A");
         boolean hasCircle = graph.checkCircle(pathWithCircle);
         System.out.println("Does the path have a circle? " + hasCircle);
+        System.out.println();
 
         // Kiểm tra xem đồ thị có phải là 1 chiều hay 2 chiều
         boolean isDirected = graph.isDirected();
         System.out.println("Is the graph directed? " + isDirected);
+        System.out.println();
     }
 }
